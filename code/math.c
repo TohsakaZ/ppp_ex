a241e91f7829f67df9e628aa7ee14869d9c7c3c7
ZhengHJ <524836851@qq.com>
2018-10-20 09:23:56 +0800

    add cheng func

---
+++ code/math.c
@@ -0,0 +1,6 @@
+#include "math.h"
+
+double cheng(double a,double b)
+{
+    return (a*b);
+}
--- code/test.c
+++ code/test.c
@@ -1,6 +1,8 @@
-#include "stdio.h"
+#include <stdio.h>
+#include "math.h"
 
 int main()
 {
+    printf("%f",cheng(1,2));
     printf("hello! world!");
 }
